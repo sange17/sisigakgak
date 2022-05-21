11d8c93a25f817520980899477c3deb0a63c5b09
inyeong <kiy7833@gmail.com>
2022-05-21 20:46:48 +0900

    :hammer: setDoctor data in FragmentAppointmentDoctor.java

--- app/src/main/java/com/example/sisigakgak/FragmentAppointmentDoctor.java
+++ app/src/main/java/com/example/sisigakgak/FragmentAppointmentDoctor.java
@@ -6,6 +6,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.NumberPicker;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -15,6 +16,7 @@ import androidx.lifecycle.ViewModelProvider;
 import java.util.ArrayList;
 
 public class FragmentAppointmentDoctor extends Fragment {
+    private SharedViewModel model;
     Button nextBtn;
     NumberPicker picker;
     String dept;
@@ -30,20 +32,16 @@ public class FragmentAppointmentDoctor extends Fragment {
         // Fragment로 불러올 xml 파일을 view로 가져옴
         View view = inflater.inflate(R.layout.fragment_appointment_doctor, null);
 
+        // 값 전달할 모델 생성
+        model = new ViewModelProvider(requireActivity()).get(SharedViewModel.class);
+
         // 이전 화면에서 선택한 값(진료과) 받아오기
         SharedViewModel model = new ViewModelProvider(requireActivity()).get(SharedViewModel.class);
         model.getSelected().observe(getViewLifecycleOwner(), item -> {
             dept = item.getDept();
 
             //의사 목록
-            String[] doctors = new String[5];
-            if(dept.equals(("재활의학과"))){
-                String[] doc1 = {"의사1", "의사2", "의사3","의사4", "의사5"};
-                doctors = doc1;
-            }else{
-                String[] doc2 = {"원의사", "김의사", "권의사", "신의사", "이의사"};
-                doctors = doc2;
-            }
+            String[] doctors = {"원의사", "김의사", "권의사", "신의사", "이의사"};
 
             // 휠뷰 스피너
             picker = view.findViewById(R.id.number_picker);
@@ -51,6 +49,7 @@ public class FragmentAppointmentDoctor extends Fragment {
             picker.setMaxValue(doctors.length-1); // 마지막 값 index
             picker.setDisplayedValues(doctors); // index에 해당하는 배열 값
             picker.setWrapSelectorWheel(false); // 휠 순환 제한
+
         });
 
         return view;
@@ -60,14 +59,20 @@ public class FragmentAppointmentDoctor extends Fragment {
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+        // 값 전달할 모델 생성
+        model = new ViewModelProvider(requireActivity()).get(SharedViewModel.class);
+
         // 다음으로 버튼
         nextBtn = view.findViewById(R.id.btn_next);
-        nextBtn.setOnClickListener(new View.OnClickListener(){
-            @Override
-            public void onClick(View view) {
-                // 다음 프래그먼트(화면) 띄우기
+        nextBtn.setOnClickListener(e -> {
+            // 다음 프래그먼트(화면)으로 값 전달
+            Item inputItem = new Item();
+            inputItem.setDoctor(picker.getDisplayedValues()[picker.getValue()]);
+            model.select(inputItem);
+//            Toast.makeText(getActivity(), inputItem.getDoctor(), Toast.LENGTH_LONG).show();
+
+            // 다음 프래그먼트(화면) 띄우기
                 ((AppointmentActivity)getActivity()).change_fragment(FragmentAppointmentMonth.newInstance());
-            }
         });
     }
 }
