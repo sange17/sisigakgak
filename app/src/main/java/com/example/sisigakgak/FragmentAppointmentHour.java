efeb9af654295eddd4743c86a980a699f8074fc2
inyeong <kiy7833@gmail.com>
2022-05-21 21:55:49 +0900

    :hammer: Add setHour in FragmentAppointmentHour.java

--- app/src/main/java/com/example/sisigakgak/FragmentAppointmentHour.java
+++ app/src/main/java/com/example/sisigakgak/FragmentAppointmentHour.java
@@ -7,10 +7,13 @@ import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.NumberPicker;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
 
 public class FragmentAppointmentHour extends Fragment {
+    private SharedViewModel model;
     Button nextBtn;
     NumberPicker picker;
 
@@ -24,21 +27,34 @@ public class FragmentAppointmentHour extends Fragment {
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
         // Fragment로 불러올 xml 파일을 view로 가져옴.
         View view = inflater.inflate(R.layout.fragment_appointment_hour, null);
-        nextBtn = view.findViewById(R.id.btn_next);
-        // 다음 프래그먼트 띄우기.
-        nextBtn.setOnClickListener(new View.OnClickListener(){
-            @Override
-            public void onClick(View view) {
-                ((AppointmentActivity)getActivity()).change_fragment(FragmentAppointmentMinute.newInstance());
-            }
-        });
 
         // 휠뷰 스피너
         picker = view.findViewById(R.id.number_picker);
         // 시 목록
-        picker.setMinValue(9);
-        picker.setMaxValue(17);
+        picker.setMinValue(9); // 처음 값
+        picker.setMaxValue(17); // 마지막 값
+        picker.setWrapSelectorWheel(false); // 휠 순환 제한
 
         return view;
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        // 값 전달할 모델 생성
+        model = new ViewModelProvider(requireActivity()).get(SharedViewModel.class);
+
+        // 다음으로 버튼
+        nextBtn = view.findViewById(R.id.btn_next);
+        nextBtn.setOnClickListener(e -> {
+            // 다음 프래그먼트(화면)으로 값 전달
+            Item inputItem = new Item();
+            inputItem.setHour(picker.getValue());
+            model.select(inputItem);
+//            Toast.makeText(getActivity(), inputItem.getHour(), Toast.LENGTH_LONG).show();
+
+            // 다음 프래그먼트(화면) 띄우기
+            ((AppointmentActivity)getActivity()).change_fragment(FragmentAppointmentMinute.newInstance());
+        });
     }
 }
